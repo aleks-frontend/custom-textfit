63cb43b86159da55cbfb07d7ef24b9b5f8e3ab26
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-01-16 15:55:18 +0100

    chore: updates to the textfitJS function

--- text-fit-new.js
+++ text-fit-new.js
@@ -1,67 +1,72 @@
-// Version 1.0.0 by Kevin Thornbloom.
-
+// jQuery version of the function
 function textfit() {
-	/*$('.textfit').each(function() {
-		$(this).css('font-size','initial');
-		$(this).wrapInner('<div class="textfit-inner" style="display:inline-block;white-space:nowrap"></div>');
-		var	fontSize = parseInt($(this).css('font-size')),
-				containerWidth = $(this).outerWidth(),
-				innerWidth = $(this).find('.textfit-inner').outerWidth(),
-				newfontSize = (containerWidth * fontSize) / innerWidth,
-				maxFont = $(this).data('textfit-max'),
-				minFont = $(this).data('textfit-min'),
-				adjust = $(this).data('textfit-adjust');
-		
-		if(adjust){ var newfontSize = newfontSize * adjust; }
+    $('.textfit').each(function () {
+        $(this).css('font-size', 'initial');
+        $(this).wrapInner('<div class="textfit-inner" style="display:inline-block;white-space:nowrap"></div>');
+        var fontSize = parseInt($(this).css('font-size')),
+            containerWidth = $(this).outerWidth(),
+            innerWidth = $(this).find('.textfit-inner').outerWidth(),
+            newfontSize = (containerWidth * fontSize) / innerWidth,
+            maxFont = $(this).data('textfit-max'),
+            minFont = $(this).data('textfit-min'),
+            adjust = $(this).data('textfit-adjust');
 
-		if (newfontSize > maxFont) {
-			var newfontSize = maxFont
-		} else if (newfontSize < minFont) {
-			var newfontSize = minFont
-		}
+        if (adjust) { var newfontSize = newfontSize * adjust; }
 
-		$(this).css('font-size', newfontSize + 'px').find('.textfit-inner').contents().unwrap();
+        if (newfontSize > maxFont) {
+            var newfontSize = maxFont
+        } else if (newfontSize < minFont) {
+            var newfontSize = minFont
+        }
 
-	});*/
+        $(this).css('font-size', newfontSize + 'px').find('.textfit-inner').contents().unwrap();
+    });
 }
 
-function textfitNew(){
-  let textfitElements = document.querySelectorAll(".textfit");
+// New Vanilla JS version of the function
+function textfitJS() {
+    const textfitElements = document.querySelectorAll(".textfit");
 
-  textfitElements.forEach((el) => {
-    el.style.fontSize = "initial";
-    wrapInnerNew(el, '<div class="textfit-inner" style="display:inline-block;white-space:nowrap"></div>');
-    el = el.querySelector(".textfit-inner");
-    
-    
-    //element.style.fontSize
-  });
+    textfitElements.forEach((container) => {
+        // Setting the initial font-size
+        container.style.fontSize = "initial";
+        
+        // Saving the initial HTML content of the container element in the originalHTML variable
+        const originalHTML = container.innerHTML;
+
+        // Wrapping the originalHTML content into new 'textfit-inner' div
+        container.innerHTML = `
+        <div class="textfit-inner" style="display:inline-block;white-space:nowrap">
+            ${originalHTML}
+        </div>`;
+        
+        const textfitInner = document.querySelector('.textfit-inner');
+        const compStyles = window.getComputedStyle(container);
+        const fontSize = parseInt(compStyles.getPropertyValue('font-size'));
+        const containerWidth = container.offsetWidth;
+        const innerWidth = textfitInner.offsetWidth;
+        let newfontSize = (containerWidth * fontSize) / innerWidth;
+        const maxFont = parseInt(container.dataset.textfitMax);
+        const minFont = parseInt(container.dataset.textfitMin);
+        const adjust = parseInt(container.dataset.textfitAdjust);
+
+        if ( adjust ) { newfontSize = newfontSize * adjust; }
+
+        if ( newfontSize > maxFont ) {
+            newfontSize = maxFont;
+        } else if ( newfontSize < minFont ) {
+            newfontSize = minFont;
+        }
+
+        // Setting container's font size to calculated value
+        container.style.fontSize = `${newfontSize}px`;
+
+        // Removing the temporary 'textfit-inner' element
+        textfitInner.remove();
+
+        // Putting back the original content into container element
+        container.innerHTML = originalHTML;
+    });
 }
 
-function wrapInnerNew(element, dom){
-  let inner = element.innerHTML;
-  element.innerHTML = "";
-  element.insertAdjacentHTML('beforeend', dom);
-  element.firstChild.innerHTML = inner;  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+textfitJS();
